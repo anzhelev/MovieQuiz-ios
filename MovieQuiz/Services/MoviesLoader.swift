f309fcbc8b56da62e511c2ee26b3927d4fbde3da
Andrey Zhelev <anzhelev@gmail.com>
2024-01-11 16:31:22 +0300

    Добавлены тесты (тема1)

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -7,31 +7,34 @@
 import Foundation
 
 struct MoviesLoader: MoviesLoadingProtocol {
-  // MARK: - NetworkClient
-  private let networkClient = NetworkClient()
-  
-  // MARK: - URL
-  private var mostPopularMoviesUrl: URL {
-    // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
-    guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
-      preconditionFailure("Unable to construct mostPopularMoviesUrl")
+    // MARK: - NetworkClient
+    private let networkClient: NetworkRouting
+    
+    init(networkClient: NetworkRouting = NetworkClient()) {
+        self.networkClient = networkClient
     }
-    return url
-  }
-  
-  func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
-    networkClient.fetch(url: mostPopularMoviesUrl) { result in
-      switch result {
-      case .success(let data):
-        do {
-          let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
-          handler(.success(mostPopularMovies))
-        } catch {
-          handler(.failure(error))
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
-      case .failure(let error):
-        handler(.failure(error))
-      }
+        return url
     }
-  }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
 }
